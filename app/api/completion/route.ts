4c5cd6a6e288c9b81031b79c66180e1086c04915
liuqingfeng <719993988@qq.com>
2024-01-29 23:36:28 +0800

    update use

--- app/api/completion/route.ts
+++ app/api/completion/route.ts
@@ -8,7 +8,8 @@ export const runtime = 'edge';
 export async function POST(req: Request) {
   // Extract the `prompt` from the body of the request
   const { prompt } = await req.json();
-  const authHeader = req.headers.get('Authorization');
+  // const authHeader = req.headers.get('Authorization');
+  const authHeader = 'sk-NKt5fonF0F563xye73D7D329E72749F49473FeA3C33b0e13'
   const openai = new OpenAI({
     apiKey: authHeader,
 
