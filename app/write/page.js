1a96b2c698336081af279eb1e1943093d113e2ff
liuqingfeng <719993988@qq.com>
2024-01-29 20:51:16 +0800

    update logos

--- app/write/page.js
+++ app/write/page.js
@@ -12,19 +12,19 @@ export default function ArticleMenu () {
     {
       title: "AI新媒体回答生成器",
       text: "AI新媒体回答生成器，一键生成简明扼要、实用性强的新媒体干货式回答文案",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/question.png",
       link: './write/huida'
     },
     {
       title: "AI新媒体问题生成器",
       text: "AI新媒体问题生成器，一键生成准确、专业、通俗的新媒体问题",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/zhihu.png",
       link: './write/wenti'
     },
     {
       title: "AI文章标题生成器",
       text: "AI文章标题生成器，一键生成引人注目的新媒体文章标题",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/headline.png",
       link: './write/biaoti'
     },
 
@@ -32,85 +32,85 @@ export default function ArticleMenu () {
     {
       title: "小学生作文写作",
       text: "您只需要输入作文的标题和需要的字数就可以帮您生成一篇感情丰富，生动真实的作文。",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/xxszw.png",
       link: './write/article'
     },
     {
       title: "AI诗歌生成器",
       text: "AI诗歌生成器，一键生成优美的诗歌",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/poem.png",
       link: './write/poets'
     },
     {
       title: "AI节日祝福语生成器",
       text: "AI节日祝福语生成器，快速生成具有温馨感和独特性的节日祝福语",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/jrzf.png",
       link: './write/fortune'
     },
     {
       title: "AI对联生成器",
       text: "AI对联生成器，根据上联内容自动生成内容呼应、对仗工整的下联",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/ddl.png",
       link: './write/duilian'
     },
     {
       title: "AI专业论文生成器",
       text: "输入论文标题和关键词，在短时间内生成高质量的专业论文内容",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/zhuanyelunwen.png",
       link: './write/lunwen'
     },
     {
       title: "AI文献综述生成器",
       text: "帮助您在短时间内轻松撰写出高质量的论文文献综述",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/zhuanyelunwen.png",
       link: './write/wenxian'
     },
     {
       title: "AI文章风格润色工具",
       text: "帮助用户快速改进文章的语言表达风格和整体质量",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/runse.png",
       link: './write/runse'
     },
     {
       title: "AI句子续写工具",
       text: "帮助用户续写句子或扩展句子，适用于写作、小说创作、广告文案等领域",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/continue.png",
       link: './write/jvzi'
     },
     {
       title: "AI种草文案生成器",
       text: "AI种草文案生成器，一键生成文笔优美、内容丰富的种草文案",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/mediazc.png",
       link: './write/zhongcao'
     },
     {
       title: "AI创意故事生成器",
       text: "AI创意故事生成器，一键生成情节生动、结构完整的创意故事内容",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/cygs.png",
       link: './write/chuangyi'
     },
     {
       title: "AI散文生成器",
       text: "AI散文生成器，一键生成平实自然、意境深远的散文作品",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/sanwen.png",
       link: './write/sanwen'
     },
     {
       title: "AI内容改写工具",
       text: "利用人工智能算法和自然语言处理技术，快速改写原文内容",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/rewrite.png",
       link: './write/neirong'
     },
     {
       title: "AI藏头诗生成器",
       text: "AI藏头诗生成器，一键生成文笔优美、富有趣味的藏头诗",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/cts.png",
       link: './write/cangtoushi'
     },
     {
       title: "AI对联生成器",
       text: "AI对联生成器，根据上联内容自动生成内容呼应、对仗工整的下联",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/ddl.png",
       link: './write/duilian'
     },
 
@@ -118,13 +118,13 @@ export default function ArticleMenu () {
     {
       title: "AI社会实践报告生成器",
       text: "帮助学生更高效、规范地撰写社会实践报告，提高报告的质量和可读性",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/shsjbg.png",
       link: './write/shijian'
     },
     {
       title: "AI教学计划生成器",
       text: "提高教学计划制定效率，减轻教师工作负担",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/jxjh.png",
       link: './write/jiaoxuejihua'
     },
 
@@ -132,19 +132,19 @@ export default function ArticleMenu () {
     {
       title: "AI日报周报生成器",
       text: "AI日报周报生成器，一键生成内容丰富的工作日报、周报",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/weekly.png",
       link: './write/zhoubao'
     },
     {
       title: "AI文章要点生成器",
       text: "AI文章要点生成器，帮助用户快速梳理文章的主旨、要点",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/ydtl.png",
       link: './write/yaodian'
     },
     {
       title: "AI文章摘要生成器",
       text: "AI文章摘要生成器，自动提取文章的主体内容，生成简洁、准确、通俗易懂的文章摘要",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/wzzy.png",
       link: './write/zhaiyao'
     },
 
@@ -152,13 +152,13 @@ export default function ArticleMenu () {
     {
       title: "AI短视频脚本生成器",
       text: "帮助用户快速改进文章的语言表达风格和整体质量",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/vlogscript.png",
       link: './write/jiaoben'
     },
     {
       title: "AI视频标题生成器",
       text: "快速生成符合要求的视频标题，无需花费大量时间和精力去思考",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/headline.png",
       link: './write/biaoti'
     },
 
@@ -166,49 +166,49 @@ export default function ArticleMenu () {
     {
       title: "AI广告语生成器",
       text: "借助AI广告语生成器，一键生成各类创意吸睛广告语",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/marketing.png",
       link: './write/guanggao'
     },
     {
       title: "AI产品亮点生成器",
       text: "AI产品亮点生成器，自动提取产品的亮点和优势，帮助用户对产品进行推广和宣传",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/cpld.png",
       link: './write/liangdian'
     },
     {
       title: "AI产品核心价值生成器",
       text: "AI产品核心价值生成器，自动识别产品的特点和亮点，确定产品的核心价值和优势",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/cphxjz.png",
       link: './write/hexinjiazhi'
     },
     {
       title: "AI产品特性描述生成器",
       text: "AI产品特性描述生成器，自动识别产品的特性和功能，确定产品的核心要素和特点",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/cptxms.png",
       link: './write/texing'
     },
     {
       title: "AI产品卖点生成器",
       text: "AI产品卖点生成器，自动生成具有吸引力和卖点的产品描述",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/cpmdll.png",
       link: './write/maidian'
     },
     {
       title: "AI电商产品简介生成器",
       text: "AI电商产品简介生成器，快速生成具有吸引力的电商产品描述和卖点文案",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/dscpjj.png",
       link: './write/jianjie'
     },
     {
       title: "AI商品评价生成器",
       text: "AI商品评价生成器，快速生成具有吸引力的好评内容，提高商品的美誉度和销量",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/spyzpj.png",
       link: './write/pingjia'
     },
     {
       title: "AI达人买家测评生成器",
       text: "帮助您在短时间内轻松撰写出高质量的评测内容",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/drmjcp.png",
       link: './write/pingce'
     },
 
@@ -216,49 +216,49 @@ export default function ArticleMenu () {
     {
       title: "AI幽默回复",
       text: "AI幽默回复，根据对方的话语自动生成诙谐幽默的回复话术",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/ymhf.png",
       link: './write/youmo'
     },
     {
       title: "AI星座占卜师",
       text: "AI星座占卜师，为用户提供星座占卜、运势预测、性格分析等服务",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/xzzb.png",
       link: './write/zhanbu'
     },
     {
       title: "AI疯狂星期四文案生成器",
       text: "AI疯狂星期四文案生成器，一键生成富有创意和吸引力的肯德基疯狂星期四文案",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/fkxqs.png",
       link: './write/fengkuang'
     },
     {
       title: "AI旅游攻略生成器",
       text: "AI旅游攻略生成器，一键生成高质量的新媒体平台旅游攻略",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/medialy.png",
       link: './write/lvyou'
     },
     {
       title: "AI邮件生成器",
       text: "AI邮件生成器，一键生成各种类型的电子邮件内容",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/email.png",
       link: './write/youjian'
     },
     {
       title: "AI打卡文案生成器",
       text: "AI打卡文案生成器，一键生成内容丰富景点打卡文案内容",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/scenicspot.png",
       link: './write/daka'
     },
     {
       title: "AI干货分享文案生成器",
       text: "AI干货分享文案生成器，一键生成形式多样、内容实用的干货分享类文案",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/ganhuo.png",
       link: './write/ganhuo'
     },
     {
       title: "AI美食探店文案生成器",
       text: "快速生成高质量的美食探店文章，提高店铺曝光率和知名度",
-      imageUrl: "/xiezuo.webp",
+      imageUrl: "/mstdwa.png",
       link: './write/tandian'
     },
   ]
@@ -322,7 +322,7 @@ export default function ArticleMenu () {
         {cards.map((card, index) => (
           <div key={index} className="card max-w-xs md:max-w-xs lg:w-1/5 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
-              <Image src={card.imageUrl} alt="Shoes" className="rounded-xl" width={500} height={500} />
+              <Image src={card.imageUrl} alt="logo" className="rounded" width={60} height={60} />
             </figure>
             <div className="card-body items-center text-center">
               <h2 className="card-title">{card.title}</h2>
