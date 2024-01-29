59e4330c4059da6a7aa7ff9f5605a43b60d6a261
liuqingfeng <719993988@qq.com>
2024-01-29 23:47:44 +0800

    update login push to  write

--- app/login/page.js
+++ app/login/page.js
@@ -67,7 +67,8 @@ export default function Login () {
       if (data.access_token) {
         localStorage.setItem('access_token', data.access_token)
         localStorage.setItem('token_type', data.token_type)
-        router.push('./bind')
+        // router.push('./bind')
+        router.push('./write')
       }
       if (response.ok) {
         // 登录成功，处理返回的数据，例如保存token
