adb6d6223de130f9c815f7d0bd218b2b4be4d839
liuqingfeng <719993988@qq.com>
2024-01-30 00:50:41 +0800

    update api

--- app/api/completion/route.ts
+++ app/api/completion/route.ts
@@ -17,7 +17,7 @@ export async function POST(req: Request) {
   });
   // Request the OpenAI API for the response based on the prompt
   const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo-1106',
+    model: 'gpt-4-1106-preview',
     stream: true,
     // a precise prompt is important for the AI to reply with the correct tokens
     messages: [
