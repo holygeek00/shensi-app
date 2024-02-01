5c9f78c2d90e326b929b86e6bfdce708585978e5
naassh <719993988@qq.com>
2024-02-02 06:34:23 +0800

    update

--- app/layout.js
+++ app/layout.js
@@ -2,17 +2,17 @@
 import { Inter } from "next/font/google"
 import "./globals.css"
 import dynamic from 'next/dynamic'
-import ReactGA from "react-ga4"
+
 const inter = Inter({ subsets: ["latin"] })
 
-ReactGA.initialize("G-0BKCT3XTPZ")
+
 export const metadata = {
   title: "深斯AI",
   description: "南昌深斯科技",
 }
 
 export default function RootLayout ({ children }) {
-  ReactGA.initialize("G-0BKCT3XTPZ")
+
   return (
     <html lang="en">
       <head>
