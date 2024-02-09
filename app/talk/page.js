67fb8afc8e71a72129f0b37af582da7e51507124
liuqingfeng <719993988@qq.com>
2024-02-09 09:34:44 +0800

    update

--- app/talk/page.js
+++ app/talk/page.js
@@ -81,6 +81,11 @@ export default function Chat () {
             messages.map(m => (
               <div key={m.id} className="bg-white w-2/3 self-center m-5">
                 <div className={m.role === 'user' ? "chat chat-end" : "chat chat-start"}>
+                  <div className="chat-image avatar">
+                    <div className="w-10 rounded-full">
+                      <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    </div>
+                  </div>
                   <div className="chat-header">
                     {m.role === 'user' ? '用户: ' : 'AI: '}
                   </div>
