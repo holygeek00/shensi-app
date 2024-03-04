a58e33f3d68bce9b180eda314023c55aba425603
zevin <3211295585@qq.com>
2024-03-04 13:30:33 +0800

    0.9.4

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,39 @@
+import TerserPlugin from 'terser-webpack-plugin'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
   images: {
-    domains: ['daisyui.com'],
+    remotePatterns:[
+        {
+            protocol: 'https',
+            hostname: 'daisyui.com',
+            pathname: '',
+        },
+    ]
+  },
+
+  webpack: (config, { dev, isServer }) => {
+    // 只在生产环境和客户端构建时修改配置
+    if (!dev && !isServer) {
+        console.log("开始进行生产环境打包")
+      config.optimization.minimizer = config.optimization.minimizer.map((plugin) => {
+        if (plugin.constructor.name === 'TerserPlugin') {
+          return new TerserPlugin({
+            ...plugin.options,
+            terserOptions: {
+              ...plugin.options.terserOptions,
+              compress: {
+                ...plugin.options.terserOptions.compress,
+                drop_console: true, // 启用删除console
+              },
+            },
+          });
+        }
+        return plugin;
+      });
+    }
+
+    return config;
   },
 }
 
