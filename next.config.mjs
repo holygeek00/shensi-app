95e65e73645a94932ffe6d94b079da8704304fa8
zevin <3211295585@qq.com>
2024-03-04 13:49:31 +0800

    0.9.5

--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,46 @@
 import TerserPlugin from 'terser-webpack-plugin'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-  images: {
-    remotePatterns:[
-        {
-            protocol: 'https',
-            hostname: 'daisyui.com',
-            pathname: '',
-        },
-    ]
-  },
-
-  webpack: (config, { dev, isServer }) => {
-    // 只在生产环境和客户端构建时修改配置
-    if (!dev && !isServer) {
-        console.log("开始进行生产环境打包")
-      config.optimization.minimizer = config.optimization.minimizer.map((plugin) => {
-        if (plugin.constructor.name === 'TerserPlugin') {
-          return new TerserPlugin({
-            ...plugin.options,
-            terserOptions: {
-              ...plugin.options.terserOptions,
-              compress: {
-                ...plugin.options.terserOptions.compress,
-                drop_console: true, // 启用删除console
-              },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'daisyui.com'
             },
-          });
-        }
-        return plugin;
-      });
-    }
+        ]
+    },
 
-    return config;
-  },
+    webpack: (config, {dev, isServer}) => {
+        console.log("仅在生产模式和客户端构建中应用")
+        // 仅在生产模式和客户端构建中应用
+        if (!dev && !isServer) {
+            // 确保TerserPlugin已经在minimizer数组中
+            if (config.optimization && config.optimization.minimizer) {
+                config.optimization.minimizer = config.optimization.minimizer.map((plugin) => {
+                    if (plugin.constructor.name === 'TerserPlugin') {
+                        // 修改TerserPlugin的配置
+                        return new TerserPlugin({
+                            ...plugin.options,
+                            terserOptions: {
+                                ...plugin.options.terserOptions,
+                                compress: {
+                                    ...plugin.options.terserOptions.compress,
+                                    // 删除console.log，保留console.error和console.warn
+                                    drop_console: true,
+                                    pure_funcs: ['console.log'],
+                                },
+                            },
+                        });
+                    }
+                    return plugin;
+                });
+            }
+        }
+
+        return config;
+    },
 }
 
 export default nextConfig
