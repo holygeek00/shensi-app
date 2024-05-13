1fb9b11ba1eb1c05a5b47f931ee0d195d7a51f27
Zevin <3211295585@qq.com>
2024-05-13 16:14:10 +0800

    修复已知问题，提升系统稳定性

--- app/write/page.js
+++ app/write/page.js
@@ -1,13 +1,14 @@
 'use client'
-import React, { useEffect, useState } from "react"
+import React, {useEffect, useState} from "react"
 import Link from 'next/link'
 import Navbar from "@/components/Navbar"
 import Image from 'next/image'
 import NavbarIndex from "@/components/NavbarIndex"
 import CategoryCardGroup from '@/components/CategoryCardGroup'
-import { ZMessage } from "@/components/ui/toast";
-import { NavTabLists } from '@/components/nav-tab-lists'
+import {ZMessage} from "@/components/ui/toast";
+import {NavTabLists} from '@/components/nav-tab-lists'
 import {Login} from '@/components/login'
+import {Notice} from "@/components/notice";
 
 export default function ArticleMenu() {
 
@@ -236,30 +237,34 @@ export default function ArticleMenu() {
 
     const [isLogin, setIsLogin] = useState(false)
 
-    const tabs = [
-        { id: 'tab1', title: '写作', content: <CategoryCardGroup categoryTitle="写作类" cards={educationLiteratureCards} /> },
-        { id: 'tab2', title: '社媒', content: <CategoryCardGroup categoryTitle="社交媒体类" cards={socialMediaCards} /> },
-        { id: 'tab3', title: '工作', content: <CategoryCardGroup categoryTitle="工作类" cards={workCards} /> },
-        { id: 'tab4', title: '视频', content: <CategoryCardGroup categoryTitle="短视频类" cards={shortVideoCards} /> },
-        { id: 'tab5', title: '电商', content: <CategoryCardGroup categoryTitle="电商类" cards={ecommerceCards} /> },
-        { id: 'tab6', title: '娱乐', content: <CategoryCardGroup categoryTitle="娱乐类" cards={entertainmentCards} /> },
-    ];
+    const tabs = [{
+        id: 'tab1', title: '写作', content: <CategoryCardGroup categoryTitle="写作类" cards={educationLiteratureCards}/>
+    }, {
+        id: 'tab2',
+        title: '社媒',
+        content: <CategoryCardGroup categoryTitle="社交媒体类" cards={socialMediaCards}/>
+    }, {id: 'tab3', title: '工作', content: <CategoryCardGroup categoryTitle="工作类" cards={workCards}/>}, {
+        id: 'tab4',
+        title: '视频',
+        content: <CategoryCardGroup categoryTitle="短视频类" cards={shortVideoCards}/>
+    }, {
+        id: 'tab5',
+        title: '电商',
+        content: <CategoryCardGroup categoryTitle="电商类" cards={ecommerceCards}/>
+    }, {id: 'tab6', title: '娱乐', content: <CategoryCardGroup categoryTitle="娱乐类" cards={entertainmentCards}/>},];
 
-    function TabComponent({ activeTab, handleTabChange }) {
-        return (
-            <div>
-                <div className="flex lg:flex-col justify-center text-center sm:mx-2 rounded lg:mt-28 lg:fixed lg:-translate-y-1/2 lg:top-1/3">
-                    {tabs.map(tab => (
-                        <a key={tab.id}
-                            className={`w-28 lg:h-16 sm:h-10 sm:py-3 lg:p-6 shadow m-2 ring-1 ring-white hover:scale-110 transition hover:bg-blue-500 lg:rounded-2xl sm:rounded cursor-pointer font-bold ${activeTab === tab.id ? 'ring-2 ring-blue-500 bg-blue-500 text-white' : ''}`}
-                            onClick={() => handleTabChange(tab.id)}>
+    function TabComponent({activeTab, handleTabChange}) {
+        return (<div>
+                <div
+                    className="flex lg:flex-col justify-center text-center sm:mx-2 rounded lg:mt-28 lg:fixed lg:-translate-y-1/2 lg:top-1/3">
+                    {tabs.map(tab => (<a key={tab.id}
+                                         className={`w-28 lg:h-16 sm:h-10 sm:py-3 lg:p-6 shadow m-2 ring-1 ring-white hover:scale-110 transition hover:bg-blue-500 lg:rounded-2xl sm:rounded cursor-pointer font-bold ${activeTab === tab.id ? 'ring-2 ring-blue-500 bg-blue-500 text-white' : ''}`}
+                                         onClick={() => handleTabChange(tab.id)}>
                             {tab.title}
-                        </a>
-                    ))}
+                        </a>))}
                 </div>
                 {tabs.find(tab => tab.id === activeTab)?.content}
-            </div>
-        );
+            </div>);
     }
 
 
@@ -269,23 +274,22 @@ export default function ArticleMenu() {
     }
 
     return (<div>
-        <Login />
-        <div className="bg-transparent w-screen h-screen overflow-y-scroll">
-            <Navbar title='深斯AI' isLogin={isLogin}></Navbar>
-            <div className="flex justify-center my-5">
-                <NavTabLists tabList={[
-                    { id: 1, name: 'AI写作', link: '/write' },
-                    { id: 2, name: 'AI对话', link: '/talk' },
-                    { id: 3, name: 'AI绘画', link: '/dalle' },
-                ]} />
+            <Login/>
+            <Notice noticeText={"网站正在维护，预计2天后正常使用(The website is under maintenance and is expected to be back up in 2 days.\n" +
+                ")"}/>
+            <div className="bg-transparent w-screen h-screen overflow-y-scroll">
+                <Navbar title='深斯AI' isLogin={isLogin}></Navbar>
+                <div className="flex justify-center my-5">
+                    <NavTabLists tabList={[{id: 1, name: 'AI写作', link: '/write'}, {
+                        id: 2,
+                        name: 'AI对话',
+                        link: '/talk'
+                    }, {id: 3, name: 'AI绘画', link: '/dalle'},]}/>
+                </div>
+
+                <TabComponent activeTab={activeTab} handleTabChange={(id) => {
+                    setActiveTab(id)
+                }}/>
             </div>
-
-            <TabComponent activeTab={activeTab} handleTabChange={(id) => {
-                setActiveTab(id)
-            }} />
-        </div>
-    </div>
-
-
-    )
+        </div>)
 }
