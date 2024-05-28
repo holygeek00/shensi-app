65ab35dc500ec6efe3cb612eef90f0117947c561
zevino <3211295585@qq.com>
2024-05-28 11:42:56 +0800

    fix bugs

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -5,7 +5,7 @@ import {jwtVerify} from "jose";
 import {getEncoding, encodingForModel, getEncodingNameForModel} from "js-tiktoken";
 
 export const runtime = 'nodejs'
-export const duration = '300s';
+export const maxDuration = '300s';
 
 export async function GET(req: Request) {
 
